88754871847bc3f4cc65a4ecc7b1f21ef515bd04
ksbisht <ksbisht941@gmail.com>
2021-12-03 19:36:35 +0530

    final aurora project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,94 @@
-TweenMax.to(".first", 1.5, {
-    delay: 0.5,
-    left: "-100%",
-    ease: Expo.easeInOut,
+document.addEventListener("DOMContentLoaded", () => {
+
+  let background = document.querySelector(".background");
+  let bgImgs = ["01.jpg", "02.jpg", "03.jpg", "04.jpg"];
+
+  let currentIndex = 0;
+
+  var anim = new hoverEffect({
+    parent: document.querySelector(".background"),
+    intensity: 0.3,
+    imagesRatio: 1080 / 1920,
+    image1: `./assets/images/${bgImgs[0]}`,
+    image2: `./assets/images/${bgImgs[1]}`,
+    displacementImage: "./assets/images/distortion.jpg",
+    hover: false,
   });
 
-  TweenMax.to(".second", 1.5, {
-    delay: 0.7,
-    left: "-100%",
-    ease: Expo.easeInOut,
+  var anim2 = new hoverEffect({
+    parent: document.querySelector(".background"),
+    intensity: 0.3,
+    imagesRatio: 1080 / 1920,
+    image1: `./assets/images/${bgImgs[1]}`,
+    image2: `./assets/images/${bgImgs[2]}`,
+    displacementImage: "./assets/images/distortion.jpg",
+    hover: false,
   });
 
-  TweenMax.to(".third", 1.5, {
-    delay: 1,
-    left: "-100%",
-    ease: Expo.easeInOut,
+  var anim3 = new hoverEffect({
+    parent: document.querySelector(".background"),
+    intensity: 0.3,
+    imagesRatio: 1080 / 1920,
+    image1: `./assets/images/${bgImgs[2]}`,
+    image2: `./assets/images/${bgImgs[3]}`,
+    displacementImage: "./assets/images/distortion.jpg",
+    hover: false,
   });
+
+  var anim4 = new hoverEffect({
+    parent: document.querySelector(".background"),
+    intensity: 0.3,
+    imagesRatio: 1080 / 1920,
+    image1: `./assets/images/${bgImgs[3]}`,
+    image2: `./assets/images/${bgImgs[0]}`,
+    displacementImage: "./assets/images/distortion.jpg",
+    hover: false,
+  });
+
+  let distortAnimations = [
+    anim,
+    anim2,
+    anim3,
+    anim4,
+  ];
+
+  function startNextDistortAnimation() {
+    let prevIndex = currentIndex;
+    currentIndex = (currentIndex + 1) % 4;
+    distortAnimations[prevIndex].next();
+    // showTextAnimation("next");
+    setTimeout(() => {
+      let canvas = background.querySelectorAll("canvas");
+      background.appendChild(canvas[0]);
+      distortAnimations[prevIndex].previous();
+    }, 1200);
+  }
+
+  const percentage = document.getElementById('percentage');
+  const progressCircle = document.getElementById('progress-circle');
+  let count = 0;
+  
+  setInterval(() => {
+    count += 1;
+    percentage.innerText = count;
+    progressCircle.style.strokeDashoffset = `${238.76 - (238.76 * count) / 100}px`;
+    
+    if (count == 100) {
+      count = 0;
+      startNextDistortAnimation();
+    }
+  }, 4000 / 100);
+
+
+  const hamburger = document.getElementById('hamburger');
+  const aside = document.getElementById('aside');
+  const close = document.getElementById('close');
+
+  hamburger.addEventListener('click', () => {
+    aside.classList.toggle('show')
+  });
+ 
+  close.addEventListener('click', () => {
+    aside.classList.toggle('show')
+  });
+});
