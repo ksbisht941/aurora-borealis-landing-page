f1f34a69d49263d12be405b65348d8fc20bc5b57
ksbisht <ksbisht941@gmail.com>
2021-12-10 18:15:31 +0530

    final

---
+++ README.md
@@ -0,0 +1,5 @@
+# Distortion Transition Effect With Circular Progress Bar And Glassmorphism Side Menu
+
+## [Watch it on youtube](https://youtu.be/RSJDwH7plsY)
+
+![Design Preview](/preview.jpg)
--- final/assets/js/script.js
+++ final/assets/js/script.js
@@ -1,94 +1,93 @@
 document.addEventListener("DOMContentLoaded", () => {
-
   let background = document.querySelector(".background");
   let bgImgs = ["01.jpg", "02.jpg", "03.jpg", "04.jpg"];
 
-  let currentIndex = 0;
+  let currentIdx = 0;
 
-  var anim = new hoverEffect({
+  var anim1 = new hoverEffect({
     parent: document.querySelector(".background"),
     intensity: 0.3,
-    imagesRatio: 1080 / 1920,
+    imagesRatio: 1800 / 1920,
     image1: `./assets/images/${bgImgs[0]}`,
     image2: `./assets/images/${bgImgs[1]}`,
     displacementImage: "./assets/images/distortion.jpg",
-    hover: false,
+    hover: false
   });
 
   var anim2 = new hoverEffect({
     parent: document.querySelector(".background"),
     intensity: 0.3,
-    imagesRatio: 1080 / 1920,
+    imagesRatio: 1800 / 1920,
     image1: `./assets/images/${bgImgs[1]}`,
     image2: `./assets/images/${bgImgs[2]}`,
     displacementImage: "./assets/images/distortion.jpg",
-    hover: false,
+    hover: false
   });
 
   var anim3 = new hoverEffect({
     parent: document.querySelector(".background"),
     intensity: 0.3,
-    imagesRatio: 1080 / 1920,
+    imagesRatio: 1800 / 1920,
     image1: `./assets/images/${bgImgs[2]}`,
     image2: `./assets/images/${bgImgs[3]}`,
     displacementImage: "./assets/images/distortion.jpg",
-    hover: false,
+    hover: false
   });
 
   var anim4 = new hoverEffect({
     parent: document.querySelector(".background"),
     intensity: 0.3,
-    imagesRatio: 1080 / 1920,
+    imagesRatio: 1800 / 1920,
     image1: `./assets/images/${bgImgs[3]}`,
     image2: `./assets/images/${bgImgs[0]}`,
     displacementImage: "./assets/images/distortion.jpg",
-    hover: false,
+    hover: false
   });
 
-  let distortAnimations = [
-    anim,
+  let distortionAnimations = [
+    anim1,
     anim2,
     anim3,
-    anim4,
+    anim4
   ];
 
-  function startNextDistortAnimation() {
-    let prevIndex = currentIndex;
-    currentIndex = (currentIndex + 1) % 4;
-    distortAnimations[prevIndex].next();
-    // showTextAnimation("next");
+  function startNextDistortionAnimation() {
+    let prevIdx = currentIdx;
+    currentIdx = (currentIdx + 1) % 4;
+    distortionAnimations[prevIdx].next();
+    
     setTimeout(() => {
       let canvas = background.querySelectorAll("canvas");
       background.appendChild(canvas[0]);
-      distortAnimations[prevIndex].previous();
+      distortionAnimations[prevIdx].previous();
     }, 1200);
   }
 
   const percentage = document.getElementById('percentage');
   const progressCircle = document.getElementById('progress-circle');
   let count = 0;
-  
+
   setInterval(() => {
     count += 1;
     percentage.innerText = count;
     progressCircle.style.strokeDashoffset = `${238.76 - (238.76 * count) / 100}px`;
-    
+
     if (count == 100) {
       count = 0;
-      startNextDistortAnimation();
+      startNextDistortionAnimation()
     }
-  }, 4000 / 100);
-
+  }, 6000 / 100);
 
   const hamburger = document.getElementById('hamburger');
   const aside = document.getElementById('aside');
   const close = document.getElementById('close');
 
   hamburger.addEventListener('click', () => {
-    aside.classList.toggle('show')
+    aside.classList.toggle('show');
   });
  
   close.addEventListener('click', () => {
-    aside.classList.toggle('show')
+    aside.classList.toggle('show');
   });
-});
+
+})
